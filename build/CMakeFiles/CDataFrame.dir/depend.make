83af6d49ef771de13e3d8016756494bdfe251cca
Sohukia Blueman <sohukia@gmail.com>
2024-03-29 09:33:10 +0100

    init project

---
+++ build/CMakeFiles/CDataFrame.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CDataFrame.
+# This may be replaced when dependencies are built.
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/sohukia/Documents/GitHub/CDataFrame
+# Build directory: C:/Users/sohukia/Documents/GitHub/CDataFrame/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
