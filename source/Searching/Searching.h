8174cce94da7724d94f3eba98340ac85d5542c16
sohukia <sohukia@gmail.com>
2024-04-25 16:10:04 +0200

    refactored column searching into Searching files

--- source/Searching/Searching.h
+++ source/Searching/Searching.h
@@ -4,5 +4,51 @@
 
 #ifndef CDATAFRAME_SEARCHING_H
 #define CDATAFRAME_SEARCHING_H
+#include "../types.h"
+
+/**
+ * @brief Counts the occurrences of a value in a column
+ * @param column: The column to search
+ * @param value: The value to count
+ * @return The number of occurrences of the value in the column
+ */
+int count_occurrences(Column *column, void *value);
+
+/**
+ * @brief Gets the position of a value in a column
+ * @param column: The column to search
+ * @param value: The value to find
+ * @param size: The size of the value
+ * @return The position of the value in the column
+ */
+int get_position(Column *column, void *value);
+
+/**
+ * @brief Counts the number of values in a column that are greater than a given value
+ * @param column: The column to search
+ * @param value: The value to compare
+ * @param size: The size of the value
+ * @return The number of values in the column that are greater than the given value
+ */
+int count_greater_than(Column *column, void *value, int size);
+
+/**
+ * @brief Counts the number of values in a column that are less than a given value
+ * @param column: The column to search
+ * @param value: The value to compare
+ * @param size: The size of the value
+ * @return The number of values in the column that are less than the given value
+ */
+int count_less_than(Column *column, void *value, int size);
+
+/**
+ * @brief Counts the number of values in a column that are equal to a given value
+ * @param column: The column to search
+ * @param value: The value to compare
+ * @param size: The size of the value
+ * @return The number of values in the column that are equal to the given value
+ */
+int count_equal_to(Column *column, void *value, int size);
+
 
 #endif //CDATAFRAME_SEARCHING_H
