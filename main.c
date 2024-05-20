a2f2f29d4491154ef6ef0062a3e0f1401bbfe4b4
Styl1st <151127732+Styl1st@users.noreply.github.com>
2024-05-20 18:20:13 +0200

    just testing

--- main.c
+++ main.c
@@ -7,33 +7,14 @@
 #include "source/types.h"
 
 int main() {
-    // Create DataFrame
+    // Create an empty DataFrame
     DataFrame df = create_empty_dataframe();
 
-    Column* column1 = create_column("Column 1", 10, INT, 0);
-    Column* column2 = create_column("Column 2", 10, FLOAT, 1);
-    Column* column3 = create_column("Column 3", 10, STRING, 2);
+    // Start the menu
+    menu(&df);
 
-    ///* Exemple of use : (juste enleve ce commentaire et tu verras)
-    // Insert columns into DataFrame
-    insert_column(&df, column1);
-    insert_column(&df, column2);
-    insert_column(&df, column3);
-
-    // Add data to columns
-    COLUMN_TYPE data_int = {INT, {.int_value = 42}};
-    COLUMN_TYPE data_float = {FLOAT, {.float_value = 3.14}};
-    COLUMN_TYPE data_string = {STRING, {.string_value = strdup("Hello, World!")}};
-
-    add_data(column1, &data_int);
-    add_data(column2, &data_float);
-    add_data(column3, &data_string);
-
-    // Print DataFrame
-    print_dataframe(&df);
-
-    // Cleanup
+    // Free the DataFrame when done
     delete_dataframe(&df);
-    //*/
+
     return 0;
 }
